07c2124f7e01d8c1a883651c8efdaa73b5286f86
Florian Wagner <florian.wagner@duke.edu>
2015-09-11 17:25:56 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -14,23 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-""" A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
 import sys
 import os
 
 from setuptools import setup, find_packages, Extension
-# To use a consistent encoding
 from codecs import open
 from os import path
 
 root = 'xlmhg'
-version = '1.0.5'
+description = 'XL-mHG: A Nonparametric Test For Enrichment in Ranked Binary Lists.'
+version = '1.1rc1'
 
 try:
 	import numpy as np # numpy is required
@@ -45,64 +38,45 @@ except ImportError:
 	sys.exit(1)
 
 here = path.abspath(path.dirname(__file__))
-description = 'XL-mHG: A Nonparametric Test For Enrichment in Ranked Binary Lists.'
 
-# Get the long description from the relevant file
 long_description = ''
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 # extensions
 ext_modules = []
-ext_modules.append(Extension(root + '.' + 'xlmHG_cython', sources= [root + os.sep + 'xlmHG_cython.pyx'], include_dirs = [np.get_include()]))
+ext_modules.append(Extension(root + '.' + 'xlmhg_cython', sources= [root + os.sep + 'xlmhg_cython.pyx'], include_dirs = [np.get_include()]))
 
 setup(
     name='xlmhg',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=version,
 
     description=description,
     long_description=long_description,
 
-    # The project's main homepage.
     url='https://github.com/flo-compbio/xlmhg',
 
-    # Author details
     author='Florian Wagner',
     author_email='florian.wagner@duke.edu',
 
-    # Choose your license
     license='GPLv3',
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
     ],
 
-    # What does your project relate to?
     keywords='statistics nonparametric enrichment test ranked binary lists',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     #packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     packages=['xlmhg'],
 
@@ -110,43 +84,21 @@ setup(
 	ext_modules = ext_modules,
 	cmdclass = {'build_ext': build_ext},
 
-	#libraries = ['xlmHG_cython.so'],
+	#libraries = [],
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],
     install_requires=['numpy','cython'],
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        #'dev': ['check-manifest'],
-        #'test': ['coverage'],
-    },
+	# development dependencies
+    #extras_require={},
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        #'sample': ['package_data.dat'],
-    },
+	# data
+    #package_data={}
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+	# data outside package
+    #data_files=[],
 
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
+	# executable scripts
     entry_points={
-        #'console_scripts': [
-        #    'extract_protein_coding_genes = goparser.extract_protein_coding_genes:main',
-        #],
+        #'console_scripts': []
     },
 )

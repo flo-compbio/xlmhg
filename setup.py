5d53d79b6144aac498187239e3dc52d238e1f84f
Florian Wagner <florian.wagner@duke.edu>
2017-01-05 16:17:37 -0500

    fix pypi tags [ci skip]

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ setup(
 
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Cython',
     ],
 

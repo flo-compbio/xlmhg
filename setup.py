b23d7e59f01ac10ab9d1d85cea8ffc81f499fbc9
Florian Wagner <florian.wagner@uchicago.edu>
2020-01-02 16:39:34 -0600

    fix appveyor [travis skip]

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ install_requires = [
 if 'READTHEDOCS' not in os.environ or \
         os.environ['READTHEDOCS'] != 'True':
     install_requires.extend([
-        'cython>=0.25.0, <1',
-        'numpy>=1.8, <2',
+        'cython>=0.25, <1',
+        'numpy>=1.15, <2',
     ])
 else:
     pass
@@ -165,9 +165,9 @@ setup(
              #'mock>=2.0.0, <3',
          ],
         'tests': [
-            'pytest>=2.8.5, <3',
+            'pytest>=2.8.5, <6',
             'pytest-cov>=2.2.1, <3',
-            'scipy>=1.4.1, <2',
+            'scipy>=1.1, <2',
         ],
     },
 
